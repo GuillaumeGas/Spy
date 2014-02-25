d12334cbee08d3fefec7e1a286166830c8d6409f
EmileCadorel <ecadorel@gmail.com>
2014-02-25 15:25:46 +0100

    ajout d'un server avec une connection basique

--- trunk/Common/Server.hh
+++ trunk/Common/Server.hh
@@ -1,6 +1,42 @@
 #ifndef _SERVER
 #define _SERVER
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <pthread.h>
+
+#include "Thread.hh"
+#include <map>
+
+class Server {
+
+public:
+
+    Server(int argc, char ** argv);
+    
+    void session();
+    void start();
+
+private:
+    
+    void load_port(int argc, char ** argv);
+    void init_sock();
+    void init_addr();
+    void do_accept();
+    void do_bind();
+    void do_listen();
+    void loop_accept();
+    //    std::map<string, Session> multi_Thread;
+    int m_port, m_sock;
+    sockaddr_in sin;
+};
 
 
 #endif
---
+++ trunk/Common/serv_main.cc
@@ -0,0 +1,6 @@
+#include "Server.hh"
+
+int main(int argc, char ** argv) {
+    Server s(argc, argv);
+    s.start();
+}
