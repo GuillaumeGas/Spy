0c171c1af68402df8df6bbcc0eae5e1a1ab5912d
EmileCadorel <ecadorel@gmail.com>
2014-02-26 14:01:17 +0100

    ajout d'un exemple de variatics thread

---
+++ trunk/example/th_main.cc
@@ -0,0 +1,25 @@
+#include "../Common/Thread.hh"
+#include <iostream>
+using namespace std;
+
+class th: public Thread<th> {
+
+public:
+    th() : Thread<th>(&th::run, this) {
+    }
+
+
+    void run() {
+	while ( 1 ) {
+	    cout << "hello " << endl;
+	}
+    }
+};
+
+int main() {
+    th t;
+    t.start();
+    t.join();
+
+    return 0;
+}
