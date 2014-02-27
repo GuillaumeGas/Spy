cb08c61bab928467bd9867f2ab070d1b304812fd
EmileCadorel <ecadorel@gmail.com>
2014-02-27 11:36:07 +0100

    exemple de message

--- trunk/example/server.cc
+++ trunk/example/server.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "../Common/Server.hh"
 #include "../Common/Serv_session.hh"
+#include "../Common/Message.hh"
 using namespace std;
 
 class session_on_server : public Serv_session {
@@ -8,12 +9,9 @@ class session_on_server : public Serv_session {
 public:
 
     session_on_server(int socket) : Serv_session(socket) {
-	message["salut"] = "1s2i4c";
-	sig_msg["salut"] = new signal<void(string)>;
-	sig_msg["salut"]->connect(boost::bind(&session_on_server::salut, this, _1));
-	sig_send["salut"] = new signal<void(string, string)>;
-	sig_send["salut"]->connect(boost::bind(&session_on_server::send_msg, this, _1, _2));
-	sig_send["salut"]->operator()("salut", "45 4 c d e a");
+	message["salut"] = new Message("salut", "1s3i", this);
+	message["salut"]->sig_recv.connect(boost::bind(&session_on_server::salut, this, _1));
+	message["salut"]->operator()("1 2 4");
     }
 
 
@@ -22,7 +20,11 @@ public:
 	stringstream ss(msg);
 	int value;
 	ss >> value;
-	cout << "recu " << value << endl;
+	cout << "recu " << value << " ";
+	ss >> value;
+	cout << value << " ";
+	ss >> value;
+	cout << value << " " << endl;
     }
 
 
