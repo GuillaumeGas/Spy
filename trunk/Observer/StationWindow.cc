f95c8a04f5f5dfcee8c6d38154fac9d2b2613681
GuillaumeGas <gas.guillaume@free.fr>
2014-03-04 19:04:46 +0100

    Création d'une fenetre avec screenshot plus grand, derniere activité détectée, boutons pour env msg, afficher processus et exec commande

---
+++ trunk/Observer/StationWindow.cc
@@ -0,0 +1,39 @@
+#include "StationWindow.hh"
+
+StationWindow::StationWindow(QString _station, QString _user) {
+
+  m_station = _station;
+  m_user    = _user;
+
+  main_layout = new QVBoxLayout;
+  buttons_layout = new QHBoxLayout;
+
+  title_label = new QLabel("<h1 align=\"center\">Poste " + m_station + " - " + m_user + "</h1>");
+  main_layout->addWidget(title_label);
+
+  img_label = new QLabel;
+  img_label->setPixmap(QPixmap("screen.bmp"));
+  main_layout->addWidget(img_label);
+  
+  activity_label = new QLabel("Activite detectee : <font color=\"red\">Firefox</font>");
+  main_layout->addWidget(activity_label);
+
+  button_sendMsg = new QPushButton("Envoyer un message");
+  buttons_layout->addWidget(button_sendMsg);
+
+  button_checkProc = new QPushButton("Afficher processus en cours");
+  buttons_layout->addWidget(button_checkProc);
+
+  button_cmd = new QPushButton("Ligne de commande");
+  buttons_layout->addWidget(button_cmd);
+
+  main_layout->addLayout(buttons_layout);
+
+  button_close = new QPushButton("Fermer");
+  main_layout->addWidget(button_close);
+
+  setLayout(main_layout);
+  setWindowIcon(QIcon("iconSPY.png"));
+
+  connect(button_close, SIGNAL(clicked()), this, SLOT(accept()));
+}
---
+++ trunk/Observer/StationWindow.hh
@@ -0,0 +1,38 @@
+#ifndef _STATION_WIN
+#define _STATION_WIN
+
+/**
+ *  Classe affichant une image taille originale du poste de l'utilisateur.
+ *  Si une activité particulière a été détectée, elle est affichée
+ *  Trois boutons principaux : envoyer un msg au user, afficher les processus en cours sur sa machine, executer une commande sur son poste
+ */
+
+#include <QDialog>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QPushButton>
+#include <QPixmap>
+
+class StationWindow : public QDialog {
+
+public:
+  StationWindow(QString _station, QString _user);
+
+private:
+  QVBoxLayout * main_layout;
+  QHBoxLayout * buttons_layout;
+
+  QLabel * title_label;
+  QLabel * img_label;
+  QLabel * activity_label;
+  QPushButton * button_sendMsg;
+  QPushButton * button_checkProc;
+  QPushButton * button_cmd;
+  QPushButton * button_close;
+
+  QString m_station;
+  QString m_user;
+};
+
+#endif
