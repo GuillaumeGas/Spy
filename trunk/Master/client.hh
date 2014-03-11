31b595ab60c34a60b73e65fb98893149a7180bdf
emile cadorel <ecadorel@gmail.com>
2014-03-11 13:46:51 +0100

    suppression des master_spy_ devenue inutile '

--- trunk/Master/client.hh
+++ trunk/Master/client.hh
@@ -8,39 +8,6 @@
 
 namespace master {
 
-    class master_spy_co : public Client_session {
-    public:	
-	master_spy_co( int socket );
-	
-	void do_begin();
-	void do_ok(std::string msg);
-	void do_err(std::string msg);
-	
-	std::string & name();
-	std::string & addr();
-	int & port();
-	bool & isset();
-
-
-    private:
-	bool m_isset;
-	std::string m_name, m_addr;
-	int m_port;
-    };
-
-    class master_spy_deco : public Client_session {
-    public:
-	master_spy_deco ( int socket );
-	void do_begin();
-	void do_ok( std::string msg );
-	void do_err( std::string msg );
-	bool & isset();
-	std::string & name();
-    private:
-	std::string m_name;
-	bool m_isset;
-    };
-
     class master_obse : public Client_session {
     public:
 	master_obse ( int socket );
