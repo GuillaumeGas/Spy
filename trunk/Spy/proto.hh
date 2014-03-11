be2a9d03544ee725b6874eb376a3edb82ea2c9eb
GuillaumeGas <gas.guillaume@free.fr>
2014-03-11 15:35:07 +0100

    mise a jour du proto : Img_Message

--- trunk/Spy/proto.hh
+++ trunk/Spy/proto.hh
@@ -10,6 +10,8 @@ public:
     message["warning"]       = new Message("warning", "1s1a", this);
     message["get_list_proc"] = new Message("get_list_proc", "1s", this); //demande la liste de processu actifs
     message["list_proc"]     = new Message("list_proc", "1s1a", this); //réponse : liste processus actifs
+    message["get_screenshot"]= new Message("get_screenshot", "1s1s", this); //demande un screenshot (param : zoom)
+    Img_message["screenshot"]    = new Message("screenshot", "1s1a", this); //réponse : screenshot
   }
 };
 
