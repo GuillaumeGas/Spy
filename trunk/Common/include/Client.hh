4fc7cabecf576ddf1c85aa91da5e2444b46bae82
GuillaumeGas <gas.guillaume@free.fr>
2014-03-13 12:17:12 +0100

    corrections

--- trunk/Common/include/Client.hh
+++ trunk/Common/include/Client.hh
@@ -21,127 +21,125 @@
 template <class T>
 class Client {
 public:
-  Client(int argc, char** argv) {
-    if(check_args(argc, argv) != -1) {
-      init_network();
-      info("Connexion etablie.");
-      m_stop = false;
+    Client(int argc, char** argv) {
+	if(check_args(argc, argv) != -1) {
+	    init_network();
+	    info("Connexion etablie.");
+	    m_stop = false;
 
-      session = new T(m_sock);
-      session->start();	    
-    } else {
-      error("Erreur.");
-      throw argc;
+	    session = new T(m_sock);
+	    session->start();	    
+	} else {
+	    error("Erreur.");
+	    throw argc;
+	}
     }
-  }
 
-  Client( std::string addr, int port ) {
-    m_port = port;
-    m_host = addr;
-    if ( !init_network() ) {
-      error("Erreur lors de la connexion");
-    } else {
-      info("Connexion etablie");
-      m_stop = false;
+    Client( std::string addr, int port ) {
+	m_port = port;
+	m_host = addr;
 
-      session = new T(m_sock);
-      session->start();
+	init_network();
+	info("Connexion etablie.");
+	m_stop = false;
+
+	session = new T(m_sock);
+	session->start();
     }
-  }
 
-  void join() {
-    session->join();
-  }
-
-
-  T & _session() {
-    return *session;
-  }
-
-  ~Client() { close(m_sock); }
+    void join() {
+	session->join();
+    }
 
 
-  int get_sock()const {
-    return m_sock;
-  }
-  std::string get_host()const {
-    return m_host;
-  }
-  bool stop()const {
-    return m_stop;
-  }
+    T & _session() {
+	return *session;
+    }
 
-  T * get_session()const {
-    return session;
-  }
+    ~Client() { close(m_sock); }
 
-  void quit() {
-    m_stop = true;
-  }
 
-  void error(std::string msg)const {
-    std::cout << msg << std::endl;
-  }
+    int get_sock()const {
+	return m_sock;
+    }
+    std::string get_host()const {
+	return m_host;
+    }
+    bool stop()const {
+	return m_stop;
+    }
 
-  void info(std::string msg)const {
-    std::cout << msg << std::endl;
-  }
+    T * get_session()const {
+	return session;
+    }
+
+    void quit() {
+	m_stop = true;
+    }
+
+    void error(std::string msg)const {
+	std::cout << msg << std::endl;
+    }
+
+    void info(std::string msg)const {
+	std::cout << msg << std::endl;
+    }
 
   
 
 private:
 
-  T * session;
+    T * session;
 
-  void init_network() {
-    m_sock = socket(AF_INET, SOCK_STREAM, 0);
-    if(m_sock == -1) {
-      error("Erreur initialisation de la socket.");
-      throw m_sock;
-    } else {
-      struct hostent * hostinfo = NULL;
-      hostinfo = gethostbyname(m_host.c_str());
-      if(hostinfo == NULL) {
-	error("Erreur hostinfo");
-	throw hostinfo;
-      } else {
-	m_sin.sin_addr   = *(struct in_addr*)hostinfo->h_addr;
-	m_sin.sin_family = AF_INET;
-	m_sin.sin_port   = htons(m_port);
+    void init_network() {
+	m_sock = socket(AF_INET, SOCK_STREAM, 0);
+	if(m_sock == -1) {
+	    error("Erreur initialisation de la socket.");
+	    throw m_sock;
+	} else {
+	    struct hostent * hostinfo = NULL;
+	    hostinfo = gethostbyname(m_host.c_str());
+	    if(hostinfo == NULL) {
+		error("Erreur hostinfo");
+		throw hostinfo;
+	    } else {
+		m_sin.sin_addr   = *(struct in_addr*)hostinfo->h_addr;
+		m_sin.sin_family = AF_INET;
+		m_sin.sin_port   = htons(m_port);
 
-	if(connect(m_sock,(struct sockaddr *)&m_sin, sizeof(struct sockaddr)) == -1) {
-	  error("Erreur lors de la connexion.");
-	  throw m_sin;
+		if(connect(m_sock,(struct sockaddr *)&m_sin, sizeof(struct sockaddr)) == -1) {
+		    error("Erreur lors de la connexion.");
+		    throw m_sin;
+		}
+
+	    }
+	}
+    }
+
+    int check_args(int argc, char** argv) {
+	m_port = 9999;
+	for(int i = 0; i < argc; i++) {
+	    if(strcmp(argv[i], "-P") == 0 && i != argc-1) {
+		m_port = atoi(argv[i+1]);
+	    }    
+	}
+    
+	m_host = "localhost";
+	for(int i = 0; i < argc; i++) {
+	    if(strcmp(argv[i], "-H") == 0 && i != argc-1) {
+		m_host = argv[i+1];
+	    }
 	}
 
-      }
-    }
-  }
-
-  int check_args(int argc, char** argv) {
-    m_port = 9999;
-    for(int i = 0; i < argc; i++) {
-      if(strcmp(argv[i], "-P") == 0 && i != argc-1) {
-	m_port = atoi(argv[i+1]);
-      }    
-    }
+	return 1;
     
-    m_host = "localhost";
-    for(int i = 0; i < argc; i++) {
-      if(strcmp(argv[i], "-H") == 0 && i != argc-1) {
-	m_host = argv[i+1];
-      }
     }
 
-    return 1;
-    
-  }
-
-  std::string m_host;
-  int m_port;
-  int m_sock;
-  struct sockaddr_in m_sin;
-  bool m_stop;
+    std::string m_host;
+    int m_port;
+    int m_sock;
+    struct sockaddr_in m_sin;
+    bool m_stop;
 };
 
 #endif
