fda3465cbc52e141c7f2041f38d6ad9c2736b230
GuillaumeGas <gas.guillaume@free.fr>
2014-03-17 16:43:16 +0100

    corrections

--- trunk/Observer/SettingProcWindow.cc
+++ trunk/Observer/SettingProcWindow.cc
@@ -1,73 +1,78 @@
 #include "SettingProcWindow.hh"
 #include "client.hh"
 
-SettingProcWindow::SettingProcWindow(QVector<QString> * vec_proc, Client<session_on_observer>* map_spy) : m_map_spy(map_spy) {
+using namespace std;
 
-  m_vec_proc = vec_proc;
+namespace observer {
+    SettingProcWindow::SettingProcWindow(QVector<QString> * vec_proc, map<string, Client<session_on_observer>* >* map_spy) : m_map_spy(map_spy) {
 
-  main_layout = new QVBoxLayout;
-  buttons_layout = new QHBoxLayout;
+	m_vec_proc = vec_proc;
 
-  title_label = new QLabel("Processus actuellement surveilles");
-  main_layout->addWidget(title_label);
+	main_layout = new QVBoxLayout;
+	buttons_layout = new QHBoxLayout;
 
-  proc_table = new QTableWidget;
-  proc_table->setRowCount(2);
-  proc_table->setColumnCount(1);
-  QStringList lst;
-  lst << "Processus";
-  proc_table->setHorizontalHeaderLabels(lst);
-  proc_table->verticalHeader()->setVisible(false);
-  proc_table->setEditTriggers(QAbstractItemView::NoEditTriggers);
-  proc_table->setSelectionBehavior(QAbstractItemView::SelectRows);
-  proc_table->setSelectionMode(QAbstractItemView::SingleSelection);
+	title_label = new QLabel("Processus actuellement surveilles");
+	main_layout->addWidget(title_label);
 
-  QHeaderView *headers = proc_table->horizontalHeader();
-  headers->setResizeMode(QHeaderView::Stretch);
+	proc_table = new QTableWidget;
+	proc_table->setRowCount(2);
+	proc_table->setColumnCount(1);
+	QStringList lst;
+	lst << "Processus";
+	proc_table->setHorizontalHeaderLabels(lst);
+	proc_table->verticalHeader()->setVisible(false);
+	proc_table->setEditTriggers(QAbstractItemView::NoEditTriggers);
+	proc_table->setSelectionBehavior(QAbstractItemView::SelectRows);
+	proc_table->setSelectionMode(QAbstractItemView::SingleSelection);
 
-  /* On rempli le tableau */
-  for(int i = 0; i < vec_proc->size(); i++) {
-    proc_table->setItem(0, i, new QTableWidgetItem((*vec_proc)[i]));
-  }
+	QHeaderView *headers = proc_table->horizontalHeader();
+	headers->setResizeMode(QHeaderView::Stretch);
 
-  main_layout->addWidget(proc_table);
+	/* On rempli le tableau */
+	for(int i = 0; i < vec_proc->size(); i++) {
+	    proc_table->setItem(0, i, new QTableWidgetItem((*vec_proc)[i]));
+	}
 
-  line_edit = new QLineEdit;
-  button_addProc = new QPushButton("Ajouter processus");
-  buttons_layout->addWidget(line_edit);
-  buttons_layout->addWidget(button_addProc);
-  main_layout->addLayout(buttons_layout);
+	main_layout->addWidget(proc_table);
 
-  button_close = new QPushButton("Fermer");
-  main_layout->addWidget(button_close);
+	line_edit = new QLineEdit;
+	button_addProc = new QPushButton("Ajouter processus");
+	buttons_layout->addWidget(line_edit);
+	buttons_layout->addWidget(button_addProc);
+	main_layout->addLayout(buttons_layout);
 
-  setLayout(main_layout);
-  resize(350, 450);
-  setWindowIcon(QIcon("iconSPY.png"));
+	button_close = new QPushButton("Fermer");
+	main_layout->addWidget(button_close);
+
+	setLayout(main_layout);
+	resize(350, 450);
+	setWindowIcon(QIcon("iconSPY.png"));
   
-  connect(button_close, SIGNAL(clicked()), this, SLOT(accept()));
-  connect(button_addProc, SIGNAL(clicked()), this, SLOT(add_to_procTable()));
-}
-
-void SettingProcWindow::add_to_procTable() {
-  QString content = line_edit->text();
-  if(content.length() > 0) {
-    if((proc_table->findItems(content, Qt::MatchExactly)).empty()) {
-      int count = proc_table->rowCount();
-      proc_table->insertRow(count);
-      proc_table->setItem(0, count, new QTableWidgetItem(content));
-      m_vec_proc->push_back(content);
-
-      /* On envoie au spy le nouveau processus à surveiller */
-      for(auto it : *m_map_spy) {
-	  it.second->(*_session().proto)["ADD_PROC"](content.toStdString());
-      }
-
-    } else {
-      QMessageBox::critical(this, "Erreur", "Le processus se trouve deja dans la liste !");
+	connect(button_close, SIGNAL(clicked()), this, SLOT(accept()));
+	connect(button_addProc, SIGNAL(clicked()), this, SLOT(add_to_procTable()));
     }
-  } else {
-    QMessageBox::critical(this, "Erreur", "Le champs est vide !");
-  }
-  line_edit->clear();
-}
+
+    void SettingProcWindow::add_to_procTable() {
+	QString content = line_edit->text();
+	if(content.length() > 0) {
+	    if((proc_table->findItems(content, Qt::MatchExactly)).empty()) {
+		int count = proc_table->rowCount();
+		proc_table->insertRow(count);
+		proc_table->setItem(0, count, new QTableWidgetItem(content));
+		m_vec_proc->push_back(content);
+
+		/* On envoie au spy le nouveau processus à surveiller */
+		for(auto it : *m_map_spy) {
+		    it.second->_session().proto->operator[]("INFO")(content.toStdString());
+		}
+
+	    } else {
+		QMessageBox::critical(this, "Erreur", "Le processus se trouve deja dans la liste !");
+	    }
+	} else {
+	    QMessageBox::critical(this, "Erreur", "Le champs est vide !");
+	}
+	line_edit->clear();
+    }
+
+};
--- trunk/Observer/SettingProcWindow.hh
+++ trunk/Observer/SettingProcWindow.hh
@@ -3,6 +3,9 @@
 
 /* Classe qui affiche une fenetre proposant à l'utilisateur d'ajouter des processus à surveiller, et affiche ceux déjà enregistrés */
 
+#include <iostream>
+#include <map>
+
 #include <QDialog>
 #include <QVBoxLayout>
 #include <QHBoxLayout>
@@ -16,31 +19,32 @@
 
 #include "../Net.hh"
 
-class SettingProcWindow : public QDialog {
+namespace observer {
+    class SettingProcWindow : public QDialog {
 
-    class session_on_observer;
+	class session_on_observer;
 
-    Q_OBJECT
+	Q_OBJECT
 
-    public:
-    SettingProcWindow(QVector<QString> * vec_proc, Client<session_on_observer>* map_spy);
+	public:
+	SettingProcWindow(QVector<QString> * vec_proc, std::map<std::string, Client<session_on_observer>* >* map_spy);
 
-private slots:
-    void add_to_procTable();
+    private slots:
+	void add_to_procTable();
 
-private:
-    QVector<QString> * m_vec_proc;
-    Client<session_on_observer>* m_map_spy;
+    private:
+	QVector<QString> * m_vec_proc;
+	std::map<std::string, Client<session_on_observer>* >* m_map_spy;
 
-    QVBoxLayout * main_layout;
-    QHBoxLayout * buttons_layout;
+	QVBoxLayout * main_layout;
+	QHBoxLayout * buttons_layout;
 
-    QLabel * title_label;
-    QTableWidget * proc_table;
-    QLineEdit * line_edit;
-    QPushButton * button_addProc;
-    QPushButton * button_close;
+	QLabel * title_label;
+	QTableWidget * proc_table;
+	QLineEdit * line_edit;
+	QPushButton * button_addProc;
+	QPushButton * button_close;
 
+    };
 };
-
 #endif
